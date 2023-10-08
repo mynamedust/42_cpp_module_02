93da84a19ca54dc7df13ca37252f924fb0e5422a
mynamedust <mynamedust@gmail.com>
2023-10-08 14:46:30 +0400

    add comments in Fixed.hpp

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -8,12 +8,14 @@ private:
 	int _value;
 	static const int _bits;
 public:
+	//Constructors
 	Fixed();
+	~Fixed();
 	Fixed(const int i);
 	Fixed(const float f);
-	~Fixed();
 	Fixed(const Fixed &f);
 	Fixed& operator=(const Fixed& f);
+	//Methods
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 	float toFloat(void) const;
